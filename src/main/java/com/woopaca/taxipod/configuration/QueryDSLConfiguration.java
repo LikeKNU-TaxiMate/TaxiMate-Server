7976c07ed452540fb3806bef5579b6b15b5ae830
jcw1031 <jcw001031@gmail.com>
2024-06-02 16:26:45 +0900

    chore: add dependencies and configure querydsl

---
+++ src/main/java/com/woopaca/taxipod/configuration/QueryDSLConfiguration.java
@@ -0,0 +1,21 @@
+package com.woopaca.taxipod.configuration;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class QueryDSLConfiguration {
+
+    private final EntityManager entityManager;
+
+    public QueryDSLConfiguration(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+}
