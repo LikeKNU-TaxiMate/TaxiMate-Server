ab0d028cf995b53c8c5417eba12ef26225319eea
jcw1031 <jcw001031@gmail.com>
2024-06-02 13:29:24 +0900

    initial commit

---
+++ src/main/java/com/woopaca/taxipod/TaxiPodApplication.java
@@ -0,0 +1,13 @@
+package com.woopaca.taxipod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxiPodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaxiPodApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/woopaca/taxipod/TaxiPodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woopaca.taxipod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxiPodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
