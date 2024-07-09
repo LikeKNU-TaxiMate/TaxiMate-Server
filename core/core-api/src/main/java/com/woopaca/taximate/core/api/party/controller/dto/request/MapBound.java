6ed68fe026159a1594201f2a4dbfc7afc0c463e5
jcw1031 <jcw001031@gmail.com>
2024-07-10 01:32:26 +0900

    rename: MapRange -> MapBound

--- core/core-api/src/main/java/com/woopaca/taximate/core/api/party/controller/dto/request/MapBound.java
+++ core/core-api/src/main/java/com/woopaca/taximate/core/api/party/controller/dto/request/MapBound.java
@@ -2,16 +2,15 @@ package com.woopaca.taximate.core.api.party.controller.dto.request;
 
 import com.woopaca.taximate.core.api.common.error.exception.InvalidMapRangeException;
 
-public record MapRange(double minLatitude, double minLongitude, double maxLatitude, double maxLongitude) {
+public record MapBound(double minLatitude, double minLongitude, double maxLatitude, double maxLongitude) {
 
-    public MapRange {
-        validateMapRange(minLatitude, minLongitude, maxLatitude, maxLongitude);
+    public MapBound {
+        validateBound(minLatitude, minLongitude, maxLatitude, maxLongitude);
     }
 
-    private void validateMapRange(double minLatitude, double minLongitude, double maxLatitude, double maxLongitude) {
+    private void validateBound(double minLatitude, double minLongitude, double maxLatitude, double maxLongitude) {
         if (minLatitude > maxLatitude || minLongitude > maxLongitude) {
             throw new InvalidMapRangeException(minLatitude, minLongitude, maxLatitude, maxLongitude);
         }
     }
-
 }
