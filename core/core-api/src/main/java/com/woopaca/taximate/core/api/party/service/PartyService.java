342425f51537bae6bfac0cb9fae5bd651472b423
jcw1031 <jcw001031@gmail.com>
2024-08-04 19:13:41 +0900

    feat: 팟 상세 조회 비즈니스 로직
    - PartyService의 getPartyDetails() 메서드
    - partyId로 팟을 조회하고 인증된 사용자와 호스트를 조회하여 반환
    - 조회수 증가 로직과 택시 경로, 비용, 소요 시간(외부 API)을 조회하는 부분은 제외

--- core/core-api/src/main/java/com/woopaca/taximate/core/api/party/service/PartyService.java
+++ core/core-api/src/main/java/com/woopaca/taximate/core/api/party/service/PartyService.java
@@ -3,23 +3,40 @@ package com.woopaca.taximate.core.api.party.service;
 import com.woopaca.taximate.core.api.party.controller.dto.request.MapBound;
 import com.woopaca.taximate.core.api.party.domain.Parties;
 import com.woopaca.taximate.core.api.party.domain.Party;
+import com.woopaca.taximate.core.api.party.domain.PartyDetails;
+import com.woopaca.taximate.core.api.party.domain.PartyFinder;
 import com.woopaca.taximate.core.api.party.domain.PartyMapFinder;
 import com.woopaca.taximate.core.api.party.model.Coordinate;
+import com.woopaca.taximate.core.api.taxi.domain.Taxi;
+import com.woopaca.taximate.core.api.user.domain.User;
+import com.woopaca.taximate.core.api.user.domain.UserFinder;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.List;
 
+@Slf4j
 @Service
 public class PartyService {
 
     private final PartyMapFinder partyMapFinder;
+    private final PartyFinder partyFinder;
+    private final UserFinder userFinder;
 
-    public PartyService(PartyMapFinder partyMapFinder) {
+    public PartyService(PartyMapFinder partyMapFinder, PartyFinder partyFinder, UserFinder userFinder) {
         this.partyMapFinder = partyMapFinder;
+        this.partyFinder = partyFinder;
+        this.userFinder = userFinder;
     }
 
+    /**
+     * 지도 범위 내의 팟 조회
+     * @param mapBound 지도 범위
+     * @return 지도 범위 내의 팟 목록
+     */
     @Transactional(readOnly = true)
     public Parties getPartiesInRange(MapBound mapBound) {
         LocalDateTime currentDateTime = LocalDateTime.now();
@@ -29,5 +46,23 @@ public class PartyService {
                 currentDateTime.minusMinutes(30)
         );
         return new Parties(partiesInRange);
+    }
+
+    /**
+     * 팟 상세 조회
+     * @param partyId 팟 ID
+     * @return 팟 상세 정보
+     */
+    @Transactional
+    public PartyDetails getPartyDetails(Long partyId) {
+        Party party = partyFinder.findParty(partyId);
+        //TODO 조회수 증가
+
+        User authenticatedUser = userFinder.findAuthenticatedUser();
+        User host = userFinder.findUser(party.hostId());
+
+        //TODO 택시 경로 및 비용, 예상 소요 시간 조회(외부 API)
+        Taxi taxi = new Taxi(Collections.emptyList(), 0, 0);
+        return new PartyDetails(party, host, taxi, authenticatedUser);
     }
 }
