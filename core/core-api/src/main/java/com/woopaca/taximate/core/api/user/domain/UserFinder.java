38eedb8a6b139882d751d89711ebb0577263d6cd
jcw1031 <jcw001031@gmail.com>
2024-08-04 19:04:27 +0900

    refactor: 사용자 조회 구현 객체 하나로 통합
    - AuthenticatedUserFinder와 UserFinder 통합
    - UserFinder에서 authenticate user를 조회하는 기능 추가

--- core/core-api/src/main/java/com/woopaca/taximate/core/api/user/domain/UserFinder.java
+++ core/core-api/src/main/java/com/woopaca/taximate/core/api/user/domain/UserFinder.java
@@ -3,6 +3,7 @@ package com.woopaca.taximate.core.api.user.domain;
 import com.woopaca.taximate.core.api.common.error.exception.NonexistentUserException;
 import com.woopaca.taximate.storage.db.core.entity.UserEntity;
 import com.woopaca.taximate.storage.db.core.repository.UserRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 import java.util.Objects;
@@ -11,17 +12,21 @@ import java.util.Objects;
 public class UserFinder {
 
     private final UserRepository userRepository;
-    private final AuthenticatedUserFinder authenticatedUserFinder;
 
-    public UserFinder(UserRepository userRepository, AuthenticatedUserFinder authenticatedUserFinder) {
+    public UserFinder(UserRepository userRepository) {
         this.userRepository = userRepository;
-        this.authenticatedUserFinder = authenticatedUserFinder;
+    }
+
+    public User findAuthenticatedUser() {
+        return (User) SecurityContextHolder.getContext()
+                .getAuthentication()
+                .getPrincipal();
     }
 
     public User findUser(Long userId) {
         UserEntity userEntity = userRepository.findById(userId)
                 .orElseThrow(() -> new NonexistentUserException(userId));
-        User authenticatedUser = authenticatedUserFinder.findAuthenticatedUser();
+        User authenticatedUser = findAuthenticatedUser();
         return User.of(userEntity, Objects.equals(userId, authenticatedUser.id()));
     }
 }
