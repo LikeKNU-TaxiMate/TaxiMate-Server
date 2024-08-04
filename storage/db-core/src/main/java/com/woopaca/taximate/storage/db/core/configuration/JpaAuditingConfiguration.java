d9e81d05bb950a869bbac399cf6c2e6033210cac
jcw1031 <jcw001031@gmail.com>
2024-08-04 13:22:12 +0900

    refactor: JPA auditing 적용
    - BaseEntity로 공통 필드 분리
      - id, createdAt, updatedAt
    - JPA auditing enable 설정

---
+++ storage/db-core/src/main/java/com/woopaca/taximate/storage/db/core/configuration/JpaAuditingConfiguration.java
@@ -0,0 +1,9 @@
+package com.woopaca.taximate.storage.db.core.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaAuditingConfiguration {
+}
