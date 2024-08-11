8ae25e2db8bdc1c6f38c1e80e8499e0f0a1a005d
jcw1031 <jcw001031@gmail.com>
2024-08-11 11:05:32 +0900

    feat: 팟을 생성한 사용자를 호스트 참여자로 등록
    - 팟을 생성한 사용자를 호스트로 등록하기 위해 참여 정보를 DB에 저장

--- core/core-api/src/main/java/com/woopaca/taximate/core/api/party/domain/ParticipationAppender.java
+++ core/core-api/src/main/java/com/woopaca/taximate/core/api/party/domain/ParticipationAppender.java
@@ -1,19 +1,35 @@
 package com.woopaca.taximate.core.api.party.domain;
 
+import com.woopaca.taximate.core.api.common.error.exception.NonexistentPartyException;
 import com.woopaca.taximate.core.api.user.domain.User;
+import com.woopaca.taximate.storage.db.core.entity.ParticipationEntity;
+import com.woopaca.taximate.storage.db.core.entity.PartyEntity;
 import com.woopaca.taximate.storage.db.core.repository.ParticipationRepository;
+import com.woopaca.taximate.storage.db.core.repository.PartyRepository;
 import org.springframework.stereotype.Component;
+
+import static com.woopaca.taximate.core.api.party.domain.Participation.ParticipationRole;
 
 @Component
 public class ParticipationAppender {
 
     private final ParticipationRepository participationRepository;
+    private final PartyRepository partyRepository;
 
-    public ParticipationAppender(ParticipationRepository participationRepository) {
+    public ParticipationAppender(ParticipationRepository participationRepository, PartyRepository partyRepository) {
         this.participationRepository = participationRepository;
+        this.partyRepository = partyRepository;
     }
 
-    public void appendHost(Party party, User user) {
-        // TODO 호스트 추가
+    public void appendHost(Long partyId, User user) {
+        PartyEntity partyEntity = partyRepository.findById(partyId)
+                .orElseThrow(() -> new NonexistentPartyException(partyId));
+        ParticipationEntity participationEntity = ParticipationEntity.builder()
+                .role(ParticipationRole.HOST.name())
+                .userId(user.id())
+                .party(partyEntity)
+                .status(Participation.ParticipationStatus.PARTICIPATING.name())
+                .build();
+        participationRepository.save(participationEntity);
     }
 }
