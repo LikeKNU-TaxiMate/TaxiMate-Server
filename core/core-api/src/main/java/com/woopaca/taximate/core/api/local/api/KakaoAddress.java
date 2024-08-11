6a01d5cc9d375896080a72b0a0cca5932f0c6b35
jcw1031 <jcw001031@gmail.com>
2024-08-11 10:57:56 +0900

    fix: KakaoAddress null 체크
    - roadAddress가 null인 경우를 확인하는 로직 추가
    - documents 자체가 비어있는 경우 예외 발생

--- core/core-api/src/main/java/com/woopaca/taximate/core/api/local/api/KakaoAddress.java
+++ core/core-api/src/main/java/com/woopaca/taximate/core/api/local/api/KakaoAddress.java
@@ -1,58 +1,75 @@
 package com.woopaca.taximate.core.api.local.api;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.woopaca.taximate.core.api.common.error.exception.NonexistentAddressException;
+import org.springframework.util.ObjectUtils;
 
 import java.util.List;
+import java.util.Objects;
 
-@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record KakaoAddress(List<Document> documents) {
+
+    public KakaoAddress {
+        if (ObjectUtils.isEmpty(documents)) {
+            throw new NonexistentAddressException();
+        }
+    }
 
     public String roadAddress() {
         Document document = documents.get(0);
-        RoadAddress roadAddress = document.roadAddress();
+        RoadAddress roadAddress = document.roadAddress;
+        if (Objects.isNull(roadAddress)) {
+            return null;
+        }
         return roadAddress.addressName;
     }
 
     public String address() {
         Document document = documents.get(0);
-        Address address = document.address();
+        Address address = document.address;
         return address.addressName;
     }
 
     public String buildingName() {
         Document document = documents.get(0);
-        RoadAddress roadAddress = document.roadAddress();
+        RoadAddress roadAddress = document.roadAddress;
+        if (Objects.isNull(roadAddress)) {
+            return null;
+        }
         return roadAddress.buildingName;
     }
 
     public String region3DepthName() {
         Document document = documents.get(0);
-        RoadAddress roadAddress = document.roadAddress();
-        return roadAddress.region3DepthName;
+        Address address = document.address;
+        return address.region3DepthName;
     }
 
     public String mainAddressNumber() {
         Document document = documents.get(0);
-        Address address = document.address();
+        Address address = document.address;
         return address.mainAddressNo;
     }
 
     public String subAddressNumber() {
         Document document = documents.get(0);
-        Address address = document.address();
+        Address address = document.address;
         return address.subAddressNo;
     }
 
+    @JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
     record Document(RoadAddress roadAddress, Address address) {
     }
 
-    record RoadAddress(String addressName, String region1DepthName, String region2DepthName, String region3DepthName,
-                       String roadName, String undergroundYn, String mainBuildingNo, String subBuildingNo,
-                       String buildingName, String zoneNo) {
+    @JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+    record RoadAddress(String addressName, String roadName, String mainBuildingNo, String subBuildingNo,
+                       String buildingName) {
     }
 
-    record Address(String addressName, String region1DepthName, String region2DepthName, String region3DepthName,
-                   String mountainYn, String mainAddressNo, String subAddressNo, String zipCode) {
+    @JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+    record Address(String addressName, @JsonAlias("region_3depth_name") String region3DepthName, String mainAddressNo,
+                   String subAddressNo) {
     }
 }
