94a50bd8accb690084730853baceb9cc7fafb77a
jcw1031 <jcw001031@gmail.com>
2024-08-11 11:04:25 +0900

    feat: 생성된 팟을 DB에 저장
    - 생성된 팟 도메인 객체를 엔티티로 변환하여 DB에 저장

--- core/core-api/src/main/java/com/woopaca/taximate/core/api/party/domain/PartyAppender.java
+++ core/core-api/src/main/java/com/woopaca/taximate/core/api/party/domain/PartyAppender.java
@@ -1,5 +1,6 @@
 package com.woopaca.taximate.core.api.party.domain;
 
+import com.woopaca.taximate.storage.db.core.entity.PartyEntity;
 import com.woopaca.taximate.storage.db.core.repository.PartyRepository;
 import org.springframework.stereotype.Component;
 
@@ -12,8 +13,22 @@ public class PartyAppender {
         this.partyRepository = partyRepository;
     }
 
-    public Long appendNew(Party newParty) {
-        // TODO 새로운 팟 추가
-        return null;
+    public Long appendNew(Party party) {
+        PartyEntity partyEntity = PartyEntity.builder()
+                .title(party.title())
+                .explanation(party.explanation())
+                .departureTime(party.departureTime())
+                .origin(party.origin())
+                .originAddress(party.originAddress())
+                .originLatitude(party.originLocation().latitude())
+                .originLongitude(party.originLocation().longitude())
+                .destination(party.destination())
+                .destinationAddress(party.destinationAddress())
+                .destinationLatitude(party.destinationLocation().latitude())
+                .destinationLongitude(party.destinationLocation().longitude())
+                .maxParticipants(party.maxParticipants())
+                .build();
+        PartyEntity savedPartyEntity = partyRepository.save(partyEntity);
+        return savedPartyEntity.getId();
     }
 }
