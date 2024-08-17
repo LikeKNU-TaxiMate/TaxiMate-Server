cb5d2483b00204e566cf09ff4a0309c683e01f22
jcw1031 <jcw001031@gmail.com>
2024-08-17 19:56:33 +0900

    chore: 예외 메시지 오탈자 수정
    - PartyAlreadyParticipatedException 예외 메시지에서 파티라고 표현한 부분 팟으로 변경

--- core/core-api/src/main/java/com/woopaca/taximate/core/api/common/error/exception/PartyAlreadyParticipatedException.java
+++ core/core-api/src/main/java/com/woopaca/taximate/core/api/common/error/exception/PartyAlreadyParticipatedException.java
@@ -4,7 +4,7 @@ import com.woopaca.taximate.core.api.common.error.ErrorType;
 
 public class PartyAlreadyParticipatedException extends BusinessException {
 
-    private static final String MESSAGE = "이미 참여한 파티입니다. partyId: %d, userId: %d";
+    private static final String MESSAGE = "이미 참여한 팟입니다. partyId: %d, userId: %d";
 
     public PartyAlreadyParticipatedException(Long partyId, Long userId) {
         super(String.format(MESSAGE, partyId, userId), ErrorType.PARTY_ALREADY_PARTICIPATED);
