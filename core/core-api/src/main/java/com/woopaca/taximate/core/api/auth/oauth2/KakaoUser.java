ad1851474acb70d8975ffd7bd9fc620d9be55bb9
jcw1031 <jcw001031@gmail.com>
2024-08-18 18:32:02 +0900

    refactor: 카카오 기본 프로필인 경우 null로 반환
    - KakaoUser 객체에서 profileImage() 호출 시 문자열이 없거나 기본 이미지인 경우 null을 반환

--- core/core-api/src/main/java/com/woopaca/taximate/core/api/auth/oauth2/KakaoUser.java
+++ core/core-api/src/main/java/com/woopaca/taximate/core/api/auth/oauth2/KakaoUser.java
@@ -2,6 +2,7 @@ package com.woopaca.taximate.core.api.auth.oauth2;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import org.springframework.util.StringUtils;
 
 @JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record KakaoUser(KakaoAccount kakaoAccount) implements OAuth2User {
@@ -13,6 +14,10 @@ public record KakaoUser(KakaoAccount kakaoAccount) implements OAuth2User {
 
     @Override
     public String profileImageUrl() {
+        if (!StringUtils.hasText(kakaoAccount.profile.profileImageUrl)
+                || kakaoAccount.profile.profileImageUrl.contains("/account_images/default_profile")) {
+            return null;
+        }
         return kakaoAccount.profile.profileImageUrl;
     }
 
