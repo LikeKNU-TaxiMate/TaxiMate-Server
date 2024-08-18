b71081cffccd15fa9987e83899bbacc50acc6872
jcw1031 <jcw001031@gmail.com>
2024-08-18 23:43:31 +0900

    config: 인증 필터 추가 및 경로 설정
    - /oauth/**, GET /api/v1/parties/**에 대한 요청은 허용, 나머지 요청은 모두 인증 필요
    - UsernamePasswordAuthenticationFilter 앞에 TokenAuthenticationFilter 추가

--- core/core-api/src/main/java/com/woopaca/taximate/core/api/common/config/SecurityConfiguration.java
+++ core/core-api/src/main/java/com/woopaca/taximate/core/api/common/config/SecurityConfiguration.java
@@ -1,8 +1,9 @@
 package com.woopaca.taximate.core.api.common.config;
 
-import com.woopaca.taximate.core.api.auth.filter.TemporaryGuestFilter;
+import com.woopaca.taximate.core.api.auth.filter.TokenAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,11 +12,16 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.filter.OncePerRequestFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+
+    private final TokenAuthenticationFilter tokenAuthenticationFilter;
+
+    public SecurityConfiguration(TokenAuthenticationFilter tokenAuthenticationFilter) {
+        this.tokenAuthenticationFilter = tokenAuthenticationFilter;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity security) throws Exception {
@@ -25,16 +31,12 @@ public class SecurityConfiguration {
                 .formLogin(AbstractHttpConfigurer::disable)
                 .logout(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(request -> request.requestMatchers("/api/v1/**").permitAll()
-                        .anyRequest().permitAll())
-                .addFilterBefore(temporaryGuestFilter(), UsernamePasswordAuthenticationFilter.class)
+                .authorizeHttpRequests(request -> request.requestMatchers("/oauth2/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/v1/parties/**").permitAll()
+                        .anyRequest().authenticated())
+                .addFilterBefore(tokenAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .exceptionHandling(exception -> exception
                         .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
                 .build();
-    }
-
-    @Bean
-    public OncePerRequestFilter temporaryGuestFilter() {
-        return new TemporaryGuestFilter();
     }
 }
