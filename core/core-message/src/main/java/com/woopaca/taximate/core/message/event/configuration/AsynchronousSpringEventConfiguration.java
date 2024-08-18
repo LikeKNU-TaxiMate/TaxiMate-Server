b8393a5d5420a8c8e91ad2b459e2fd815111c614
jcw1031 <jcw001031@gmail.com>
2024-08-18 15:39:53 +0900

    refactor: 이벤트 리스너가 비동기로 실행될 수 있도록 설정
    - ApplicationEventMulticaster 구현체를 빈으로 등록
    - SimpleAsyncTaskExecutor() 사용

---
+++ core/core-message/src/main/java/com/woopaca/taximate/core/message/event/configuration/AsynchronousSpringEventConfiguration.java
@@ -0,0 +1,18 @@
+package com.woopaca.taximate.core.message.event.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.ApplicationEventMulticaster;
+import org.springframework.context.event.SimpleApplicationEventMulticaster;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+
+@Configuration
+public class AsynchronousSpringEventConfiguration {
+
+    @Bean
+    public ApplicationEventMulticaster applicationEventMulticaster() {
+        SimpleApplicationEventMulticaster eventMulticaster = new SimpleApplicationEventMulticaster();
+        eventMulticaster.setTaskExecutor(new SimpleAsyncTaskExecutor());
+        return eventMulticaster;
+    }
+}
