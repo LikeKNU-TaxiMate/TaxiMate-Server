0915dd77dec14a18ea0f55de311d0a81271caf77
jcw1031 <jcw001031@gmail.com>
2024-08-18 15:33:18 +0900

    feat: 팟 참여 요청 API 처리
    - ParticipationController의 participateParty() 메서드와 매핑
    - POST /api/v1/participation에 대한 요청 처리
    - query parameter로 partyId를 받음

---
+++ core/core-api/src/main/java/com/woopaca/taximate/core/api/party/controller/ParticipationController.java
@@ -0,0 +1,27 @@
+package com.woopaca.taximate.core.api.party.controller;
+
+import com.woopaca.taximate.core.api.common.model.ApiResults;
+import com.woopaca.taximate.core.api.common.model.ApiResults.ApiResponse;
+import com.woopaca.taximate.core.api.party.controller.dto.response.ParticipatePartyResponse;
+import com.woopaca.taximate.core.api.party.service.ParticipationService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/v1/participation")
+@RestController
+public class ParticipationController {
+
+    private final ParticipationService participationService;
+
+    public ParticipationController(ParticipationService participationService) {
+        this.participationService = participationService;
+    }
+
+    @PostMapping
+    public ApiResponse<ParticipatePartyResponse> participateParty(@RequestParam("partyId") Long partyId) {
+        Long participatedPartyId = participationService.participateParty(partyId);
+        return ApiResults.success(new ParticipatePartyResponse(participatedPartyId));
+    }
+}
