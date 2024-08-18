eaa27a4ed085106625db9a1f741a801b76a7baf4
jcw1031 <jcw001031@gmail.com>
2024-08-18 18:06:20 +0900

    refactor: 필요한 생성자 및 메서드 추가
    - UserEntity에 생성자를 추가하고 lombok @Builder 사용
    - UserRepository에 email로 검색할 수 있는 findByEmail() 메서드 추가

--- storage/db-core/src/main/java/com/woopaca/taximate/storage/db/core/repository/UserRepository.java
+++ storage/db-core/src/main/java/com/woopaca/taximate/storage/db/core/repository/UserRepository.java
@@ -4,6 +4,10 @@ import com.woopaca.taximate.storage.db.core.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    Optional<UserEntity> findByEmail(String email);
 }
