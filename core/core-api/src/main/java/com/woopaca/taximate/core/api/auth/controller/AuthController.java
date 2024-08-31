fd2dfb989b486a87074a59807996c2c22dbb738f
jcw1031 <jcw001031@gmail.com>
2024-08-31 22:38:24 +0900

    feature: 인증 컨트롤러 추가
    - GET /auth/tokens 요청을 처리하는 핸들러
    - 토큰 재발급 요청을 처리

---
+++ core/core-api/src/main/java/com/woopaca/taximate/core/api/auth/controller/AuthController.java
@@ -0,0 +1,35 @@
+package com.woopaca.taximate.core.api.auth.controller;
+
+import com.woopaca.taximate.core.api.auth.controller.dto.response.TokensResponse;
+import com.woopaca.taximate.core.api.auth.controller.utils.RefreshTokenCookies;
+import com.woopaca.taximate.core.api.auth.model.Tokens;
+import com.woopaca.taximate.core.api.auth.service.AuthService;
+import com.woopaca.taximate.core.api.common.model.ApiResults;
+import com.woopaca.taximate.core.api.common.model.ApiResults.ApiResponse;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/auth")
+@RestController
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @GetMapping("/tokens")
+    public ApiResponse<TokensResponse> reissueTokens(@CookieValue("refreshToken") String refreshToken, HttpServletResponse response) {
+        Tokens tokens = authService.reissueTokensBy(refreshToken);
+
+        Cookie refreshTokenCookie = RefreshTokenCookies.generateCookie(tokens.refreshToken());
+        response.addCookie(refreshTokenCookie);
+
+        return ApiResults.success(new TokensResponse(tokens.accessToken()));
+    }
+}
