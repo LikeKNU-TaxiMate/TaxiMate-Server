4cff1798690710ea292917c4698f47fbe01cb714
jcw1031 <jcw001031@gmail.com>
2024-08-31 17:38:37 +0900

    refactor: KeyValueRepository 리팩토링
    - KeyValueRepository에 주석 추가
    - RedisKeyValueRepository에서 set(), remove() 메서드 비동기 적용

--- storage/db-nosql/src/main/java/com/woopaca/taximate/storage/db/nosql/repository/KeyValueRepository.java
+++ storage/db-nosql/src/main/java/com/woopaca/taximate/storage/db/nosql/repository/KeyValueRepository.java
@@ -4,11 +4,35 @@ import java.time.Duration;
 
 public interface KeyValueRepository {
 
+    /**
+     * 키-값 저장
+     * @param key null이 아닌 문자열
+     * @param value null이 아닌 문자열
+     * @see #set(String, String, Duration)
+     */
     void set(String key, String value);
 
+    /**
+     * 키-값 저장
+     * @param key null이 아닌 문자열
+     * @param value null이 아닌 문자열
+     * @param duration null일 수 없음
+     *
+     * @see #set(String, String)
+     * @see java.time.Duration
+     */
     void set(String key, String value, Duration duration);
 
+    /**
+     * 키로 값 조회
+     * @param key null이 아닌 문자열
+     * @return null일 수 있음
+     */
     String get(String key);
 
+    /**
+     * 키로 값 삭제
+     * @param key null이 아닌 문자열
+     */
     void remove(String key);
 }
--- storage/db-nosql/src/main/java/com/woopaca/taximate/storage/db/nosql/repository/RedisKeyValueRepository.java
+++ storage/db-nosql/src/main/java/com/woopaca/taximate/storage/db/nosql/repository/RedisKeyValueRepository.java
@@ -1,10 +1,13 @@
 package com.woopaca.taximate.storage.db.nosql.repository;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Repository;
 
 import java.time.Duration;
 
+@Slf4j
 @Repository
 public class RedisKeyValueRepository implements KeyValueRepository {
 
@@ -14,11 +17,13 @@ public class RedisKeyValueRepository implements KeyValueRepository {
         this.redisTemplate = redisTemplate;
     }
 
+    @Async
     @Override
     public void set(String key, String value) {
         redisTemplate.opsForValue().set(key, value);
     }
 
+    @Async
     @Override
     public void set(String key, String value, Duration duration) {
         redisTemplate.opsForValue().set(key, value, duration);
@@ -29,6 +34,7 @@ public class RedisKeyValueRepository implements KeyValueRepository {
         return redisTemplate.opsForValue().get(key);
     }
 
+    @Async
     @Override
     public void remove(String key) {
         redisTemplate.delete(key);
