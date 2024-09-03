26203a126e317c340997dc536bd8691b07318654
jcw1031 <jcw001031@gmail.com>
2024-09-04 00:48:00 +0900

    fix: 시작 스크립트 수정 2

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,6 +2,7 @@
 
 ROOT_PATH="/home/ubuntu"
 DEPLOY_PATH="/home/ubuntu/deploy"
+ORIGIN_JAR="$DEPLOY_PATH/core/core-api/build/libs/*.jar"
 
 APP_LOG="$DEPLOY_PATH/application.log"
 ERROR_LOG="$DEPLOY_PATH/error.log"
@@ -10,11 +11,9 @@ START_LOG="$DEPLOY_PATH/start.log"
 NOW=$(date +%c)
 
 echo "[$NOW] Copy $ROOT_PATH" >> $START_LOG
-cp $DEPLOY_PATH/core/core-api/build/libs/*.jar $ROOT_PATH
+cp $ORIGIN_JAR $ROOT_PATH
 
 JAR=$(find $ROOT_PATH -name "*.jar" | head -n 1)
-
-sleep 3
 
 echo "[$NOW] Run $JAR" >> $START_LOG
 nohup java -jar $JAR --spring.profiles.active=develop > $APP_LOG 2> $ERROR_LOG &
