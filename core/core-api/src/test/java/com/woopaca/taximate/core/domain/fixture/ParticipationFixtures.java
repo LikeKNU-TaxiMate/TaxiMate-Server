d1a6cd211266ee62c3577223ae886543b5221d92
jcw1031 <jcw001031@gmail.com>
2024-09-11 12:22:58 +0900

    refactor: 테스트 fixture 클래스 리팩토링
    - ParticipationFixtures, PartyFixtures, UserFixtures 사용하지 않는 메서드 제거
    - AddressFixtures 추가

--- core/core-api/src/test/java/com/woopaca/taximate/core/domain/fixture/ParticipationFixtures.java
+++ core/core-api/src/test/java/com/woopaca/taximate/core/domain/fixture/ParticipationFixtures.java
@@ -1,16 +1,21 @@
 package com.woopaca.taximate.core.domain.fixture;
 
+import com.woopaca.taximate.core.domain.party.Participation.ParticipationRole;
+import com.woopaca.taximate.core.domain.party.Participation.ParticipationStatus;
 import com.woopaca.taximate.storage.db.core.entity.ParticipationEntity;
 import com.woopaca.taximate.storage.db.core.entity.PartyEntity;
 
 public final class ParticipationFixtures {
 
+    private ParticipationFixtures() {
+    }
+
     public static ParticipationEntity createParticipationEntityWith(PartyEntity partyEntity, Long userId) {
         return ParticipationEntity.builder()
                 .party(partyEntity)
                 .userId(userId)
-                .role("PARTICIPANT")
-                .status("PARTICIPATING")
+                .role(ParticipationRole.PARTICIPANT.name())
+                .status(ParticipationStatus.PARTICIPATING.name())
                 .build();
     }
 }
