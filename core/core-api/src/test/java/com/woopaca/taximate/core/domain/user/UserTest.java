307317e79e4ee89ff696727e3959f05445806735
jcw1031 <jcw001031@gmail.com>
2024-09-12 18:45:08 +0900

    test: User 도메인 테스트 작성
    * isCurrentUser() 메서드 테스트
    * fromEntity() 메서드 테스트

---
+++ core/core-api/src/test/java/com/woopaca/taximate/core/domain/user/UserTest.java
@@ -0,0 +1,68 @@
+package com.woopaca.taximate.core.domain.user;
+
+import com.woopaca.taximate.core.domain.auth.AuthenticatedUserHolder;
+import com.woopaca.taximate.core.domain.fixture.UserFixtures;
+import com.woopaca.taximate.core.domain.user.User.AccountStatus;
+import com.woopaca.taximate.core.domain.user.User.OAuth2Provider;
+import com.woopaca.taximate.storage.db.core.entity.UserEntity;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class UserTest {
+
+    @Nested
+    class isCurrentUser_메서드는 {
+
+        @Test
+        void 현재_사용자와_같은_사용자인_경우_true를_반환한다() {
+            //given
+            User user = UserFixtures.createUser(1L);
+            AuthenticatedUserHolder.setAuthenticatedUser(user);
+
+            //when
+            boolean isCurrentUser = user.isCurrentUser();
+
+            //then
+            assertTrue(isCurrentUser);
+        }
+
+        @Test
+        void 현재_사용자와_다른_사용자인_경우_false를_반환한다() {
+            //given
+            User user = UserFixtures.createUser(1L);
+            User other = UserFixtures.createUser(2L);
+            AuthenticatedUserHolder.setAuthenticatedUser(other);
+
+            //when
+            boolean isCurrentUser = user.isCurrentUser();
+
+            //then
+            assertFalse(isCurrentUser);
+        }
+    }
+
+    @Nested
+    class fromEntity_메서드는 {
+
+        @Test
+        void UserEntity를_User로_변환한다() {
+            //given
+            UserEntity userEntity = UserFixtures.createUserEntityWith("test1");
+
+            //when
+            User user = User.fromEntity(userEntity);
+
+            //then
+            assertThat(user.getId()).isEqualTo(userEntity.getId());
+            assertThat(user.getEmail()).isEqualTo(userEntity.getEmail());
+            assertThat(user.getNickname()).isEqualTo(userEntity.getNickname());
+            assertThat(user.getProfileImage()).isEqualTo(userEntity.getProfileImage());
+            assertThat(user.getProvider()).isEqualTo(OAuth2Provider.valueOf(userEntity.getProvider()));
+            assertThat(user.getStatus()).isEqualTo(AccountStatus.valueOf(userEntity.getStatus()));
+        }
+    }
+}
