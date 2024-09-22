3b0652ddc733b6f2c6429ed06b5c70c4afdd4edb
jcw1031 <jcw001031@gmail.com>
2024-09-22 21:39:34 +0900

    refactor: 이벤트 리스너 TaskExecutor 설정 변경
    * SimpleAsyncTaskExecutor 대신 ThreadPoolTaskExecutor를 사용하도록 변경

--- core/core-domain/src/main/java/com/woopaca/taximate/core/domain/event/config/AsynchronousSpringEventConfiguration.java
+++ core/core-domain/src/main/java/com/woopaca/taximate/core/domain/event/config/AsynchronousSpringEventConfiguration.java
@@ -1,20 +1,36 @@
 package com.woopaca.taximate.core.domain.event.config;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.task.ThreadPoolTaskExecutorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.ApplicationEventMulticaster;
 import org.springframework.context.event.SimpleApplicationEventMulticaster;
-import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.scheduling.annotation.EnableAsync;
+
+import java.time.Duration;
 
 @EnableAsync
 @Configuration
 public class AsynchronousSpringEventConfiguration {
 
     @Bean
-    public ApplicationEventMulticaster applicationEventMulticaster() {
+    public ApplicationEventMulticaster applicationEventMulticaster(@Qualifier("taskExecutor") TaskExecutor taskExecutor) {
         SimpleApplicationEventMulticaster eventMulticaster = new SimpleApplicationEventMulticaster();
-        eventMulticaster.setTaskExecutor(new SimpleAsyncTaskExecutor());
+        eventMulticaster.setTaskExecutor(taskExecutor);
         return eventMulticaster;
+    }
+
+    @Bean
+    public TaskExecutor taskExecutor() {
+        return new ThreadPoolTaskExecutorBuilder()
+                .corePoolSize(10)
+                .maxPoolSize(50)
+                .queueCapacity(200)
+                .threadNamePrefix("EventHandler-")
+                .keepAlive(Duration.ofSeconds(60))
+                .allowCoreThreadTimeOut(true)
+                .build();
     }
 }
