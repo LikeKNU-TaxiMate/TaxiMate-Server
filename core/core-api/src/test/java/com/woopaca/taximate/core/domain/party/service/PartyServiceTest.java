fbda78d843e33271f406ab6e62acf334709f83ca
jcw1031 <jcw001031@gmail.com>
2024-09-26 09:59:31 +0900

    test: PartyService 테스트 코드 작성
    * 정상적인 경우에 대해서만 작성
    * getPartiesInRange() 메서드 테스트
    * createParty() 메서드 테스트

--- core/core-api/src/test/java/com/woopaca/taximate/core/domain/party/service/PartyServiceTest.java
+++ core/core-api/src/test/java/com/woopaca/taximate/core/domain/party/service/PartyServiceTest.java
@@ -1,11 +1,21 @@
 package com.woopaca.taximate.core.domain.party.service;
 
+import com.woopaca.taximate.core.domain.auth.AuthenticatedUserHolder;
 import com.woopaca.taximate.core.domain.fixture.PartyFixtures;
+import com.woopaca.taximate.core.domain.fixture.UserFixtures;
+import com.woopaca.taximate.core.domain.local.AddressAllocator;
+import com.woopaca.taximate.core.domain.party.Participation.ParticipationRole;
+import com.woopaca.taximate.core.domain.party.Participation.ParticipationStatus;
 import com.woopaca.taximate.core.domain.party.Parties;
+import com.woopaca.taximate.core.domain.party.Party;
 import com.woopaca.taximate.core.domain.party.model.Coordinate;
 import com.woopaca.taximate.core.domain.party.model.MapBound;
+import com.woopaca.taximate.storage.db.core.entity.ParticipationEntity;
+import com.woopaca.taximate.storage.db.core.entity.PartyEntity;
+import com.woopaca.taximate.storage.db.core.repository.ParticipationRepository;
 import com.woopaca.taximate.storage.db.core.repository.PartyRepository;
 import org.h2gis.functions.factory.H2GISFunctions;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
@@ -14,6 +24,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.Connection;
@@ -31,6 +42,11 @@ class PartyServiceTest {
     private PartyService partyService;
     @Autowired
     private PartyRepository partyRepository;
+    @Autowired
+    private ParticipationRepository participationRepository;
+
+    @MockBean
+    private AddressAllocator addressAllocator;
 
     @Nested
     class getPartiesInRange_메서드는 {
@@ -55,6 +71,48 @@ class PartyServiceTest {
 
             //then
             assertThat(partiesInRange.stream()).hasSize(1);
+        }
+
+        @Test
+        void 주어진_좌표_범위_내에_있는_팟이_없으면_빈_팟_목록을_반환한다() {
+            //given
+            partyRepository.save(PartyFixtures.createPartyEntityWith(Coordinate.of(37.123456, 127.123456)));
+            partyRepository.save(PartyFixtures.createPartyEntityWith(Coordinate.of(36.123456, 127.123456)));
+            partyRepository.save(PartyFixtures.createPartyEntityWith(Coordinate.of(37.123456, 128.123456)));
+
+            //when
+            MapBound mapBound = new MapBound(39.0, 129.0, 40.0, 130.0);
+            Parties partiesInRange = partyService.getPartiesInRange(mapBound);
+
+            //then
+            assertThat(partiesInRange.stream()).isEmpty();
+        }
+    }
+
+    @Nested
+    class createParty_메서드는 {
+
+        @Test
+        void 새로운_팟을_생성하고_참여_정보를_생성한다() {
+            //given
+            Party party = PartyFixtures.createParty();
+            AuthenticatedUserHolder.setAuthenticatedUser(UserFixtures.createUser(1L));
+
+            //when
+            Long partyId = partyService.createParty(party);
+
+            //then
+            PartyEntity partyEntity = partyRepository.findById(partyId)
+                    .orElseThrow();
+            ParticipationEntity participationEntity = participationRepository.findByPartyId(partyId)
+                    .get(0);
+            Assertions.assertAll(
+                    () -> assertThat(partyEntity.getId()).isEqualTo(partyId),
+                    () -> assertThat(participationEntity).isNotNull(),
+                    () -> assertThat(participationEntity.getUserId()).isEqualTo(1L),
+                    () -> assertThat(participationEntity.getRole()).isEqualTo(ParticipationRole.HOST.name()),
+                    () -> assertThat(participationEntity.getStatus()).isEqualTo(ParticipationStatus.PARTICIPATING.name())
+            );
         }
     }
 }
