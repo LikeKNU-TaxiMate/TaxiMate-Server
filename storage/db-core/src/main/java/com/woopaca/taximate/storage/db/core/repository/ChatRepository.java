11900c2815ff47e2c4dae6245aa24b9ddf7c4484
jcw1031 <jcw001031@gmail.com>
2024-10-04 17:57:10 +0900

    refactor: 채팅 ID 생성기 초기화
    * 스프링 빈 초기화 시 마지막 채팅 ID를 조회해 초기 ID값 세팅

--- storage/db-core/src/main/java/com/woopaca/taximate/storage/db/core/repository/ChatRepository.java
+++ storage/db-core/src/main/java/com/woopaca/taximate/storage/db/core/repository/ChatRepository.java
@@ -1,9 +1,14 @@
 package com.woopaca.taximate.storage.db.core.repository;
 
 import com.woopaca.taximate.storage.db.core.entity.ChatEntity;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ChatRepository extends JpaRepository<ChatEntity, Long> {
+
+    Optional<ChatEntity> findTopBy(Sort sort);
 }
