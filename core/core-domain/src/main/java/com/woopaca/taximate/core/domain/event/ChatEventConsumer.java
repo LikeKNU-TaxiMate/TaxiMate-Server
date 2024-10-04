537a1c36f9de69e7f5c0efd9857019326802e239
jcw1031 <jcw001031@gmail.com>
2024-10-04 16:16:18 +0900

    feature: 채팅 읽음 기록 처리
    * 메시지 전송 시 메시지 타입에 따라 처리
      - 참여 메시지의 경우 채팅 읽음 기록 생성
      - 나가기 메시지의 경우 채팅 읽음 기록 제거
      - 일반 메시지의 경우 채팅 읽음 기록 갱신

--- core/core-domain/src/main/java/com/woopaca/taximate/core/domain/event/ChatEventConsumer.java
+++ core/core-domain/src/main/java/com/woopaca/taximate/core/domain/event/ChatEventConsumer.java
@@ -1,6 +1,8 @@
 package com.woopaca.taximate.core.domain.event;
 
+import com.woopaca.taximate.core.domain.chat.Chat;
 import com.woopaca.taximate.core.domain.chat.ChatAppender;
+import com.woopaca.taximate.core.domain.chat.ChatReadRecorder;
 import com.woopaca.taximate.core.domain.event.dto.ChatEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
@@ -9,13 +11,24 @@ import org.springframework.stereotype.Component;
 public class ChatEventConsumer {
 
     private final ChatAppender chatAppender;
+    private final ChatReadRecorder chatReadRecorder;
 
-    public ChatEventConsumer(ChatAppender chatAppender) {
+    public ChatEventConsumer(ChatAppender chatAppender, ChatReadRecorder chatReadRecorder) {
         this.chatAppender = chatAppender;
+        this.chatReadRecorder = chatReadRecorder;
     }
 
     @EventListener
     public void handleChatEvent(ChatEvent chatEvent) {
-        chatAppender.appendChat(chatEvent.chat());
+        Chat newChat = chatAppender.appendNew(chatEvent.chat());
+        if (newChat.isParticipateMessage()) {
+            chatReadRecorder.createReadHistory(newChat);
+            return;
+        }
+        if (newChat.isLeaveMessage()) {
+            chatReadRecorder.removeReadHistory(newChat);
+            return;
+        }
+        chatReadRecorder.recordReadHistory(newChat);
     }
 }
