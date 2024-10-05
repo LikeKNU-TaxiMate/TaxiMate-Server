2c680a09494960b0c505e2a3bd2890ea9493ab1b
jcw1031 <jcw001031@gmail.com>
2024-10-05 20:04:17 +0900

    refactor: 채팅 저장 쿼리 최적화
    * Generated ID가 아니기 때문에 JpaRepository를 사용하면 SELECT -> INSERT 순서로 쿼리가 2번 실행
    * EntityManager의 persist()를 직접 호출해 바로 INSERT만 나가도록 변경

--- core/core-domain/src/main/java/com/woopaca/taximate/core/domain/chat/ChatAppender.java
+++ core/core-domain/src/main/java/com/woopaca/taximate/core/domain/chat/ChatAppender.java
@@ -3,28 +3,30 @@ package com.woopaca.taximate.core.domain.chat;
 import com.woopaca.taximate.core.domain.user.User;
 import com.woopaca.taximate.storage.db.core.entity.ChatEntity;
 import com.woopaca.taximate.storage.db.core.entity.UserEntity;
-import com.woopaca.taximate.storage.db.core.repository.ChatRepository;
 import com.woopaca.taximate.storage.db.core.repository.UserRepository;
+import jakarta.persistence.EntityManager;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Component
 public class ChatAppender {
 
-    private final ChatRepository chatRepository;
     private final UserRepository userRepository;
+    private final EntityManager entityManager;
 
-    public ChatAppender(ChatRepository chatRepository, UserRepository userRepository) {
-        this.chatRepository = chatRepository;
+    public ChatAppender(UserRepository userRepository, EntityManager entityManager) {
         this.userRepository = userRepository;
+        this.entityManager = entityManager;
     }
 
+    @Transactional
     public void appendNew(Chat chat) {
         User sender = chat.getSender();
         UserEntity userEntity = userRepository.findById(sender.getId())
                 .orElse(null);
         ChatEntity chatEntity = chat.toEntity(userEntity);
-        chatRepository.save(chatEntity);
+        entityManager.persist(chatEntity);
     }
 }
