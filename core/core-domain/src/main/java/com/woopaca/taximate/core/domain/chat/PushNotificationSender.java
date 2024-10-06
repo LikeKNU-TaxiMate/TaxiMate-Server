3e21bb8f29d087b5f063e076b1d35afe5e6813d1
jcw1031 <jcw001031@gmail.com>
2024-10-06 23:36:43 +0900

    feature: 푸시 알림 전송 기능 구현

--- core/core-domain/src/main/java/com/woopaca/taximate/core/domain/chat/PushNotificationSender.java
+++ core/core-domain/src/main/java/com/woopaca/taximate/core/domain/chat/PushNotificationSender.java
@@ -1,4 +1,35 @@
 package com.woopaca.taximate.core.domain.chat;
 
-public interface PushNotificationSender {
+import com.woopaca.taximate.core.domain.notification.PushNotification;
+import com.woopaca.taximate.core.domain.notification.PushNotificationClient;
+import com.woopaca.taximate.core.domain.notification.PushTokenFinder;
+import com.woopaca.taximate.core.domain.party.Party;
+import com.woopaca.taximate.core.domain.user.User;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class PushNotificationSender {
+
+    private final PushNotificationClient pushNotificationClient;
+    private final PushTokenFinder pushTokenFinder;
+
+    public PushNotificationSender(PushNotificationClient pushNotificationClient, PushTokenFinder pushTokenFinder) {
+        this.pushNotificationClient = pushNotificationClient;
+        this.pushTokenFinder = pushTokenFinder;
+    }
+
+    /**
+     * 채팅 푸시 알림 전송
+     * @param chat 채팅
+     */
+    public void send(Chat chat, List<User> receivers) {
+        Party party = chat.getParty();
+        User sender = chat.getSender();
+        PushNotification pushNotification = new PushNotification(party.getTitle(), sender.getNickname(),
+                chat.getMessage(), 0);
+        List<String> tokens = pushTokenFinder.findTokens(receivers);
+        pushNotificationClient.sendNotifications(pushNotification, tokens);
+    }
 }
