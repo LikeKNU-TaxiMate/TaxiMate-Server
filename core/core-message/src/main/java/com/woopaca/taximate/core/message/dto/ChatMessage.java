88fabb123bee244a89c158e17c88c8ab813cc0b1
jcw1031 <jcw001031@gmail.com>
2024-10-07 19:58:06 +0900

    fix: 채팅 WebSocket 메시지 필드 추가
    * 채팅 ID 추가

--- core/core-message/src/main/java/com/woopaca/taximate/core/message/dto/ChatMessage.java
+++ core/core-message/src/main/java/com/woopaca/taximate/core/message/dto/ChatMessage.java
@@ -2,19 +2,22 @@ package com.woopaca.taximate.core.message.dto;
 
 import com.woopaca.taximate.core.domain.chat.Chat;
 import com.woopaca.taximate.core.domain.chat.MessageType;
+import com.woopaca.taximate.core.domain.party.Party;
 import com.woopaca.taximate.core.domain.user.User;
 import lombok.Builder;
 
 import java.time.LocalDateTime;
 
 @Builder
-public record ChatMessage(Long partyId, String partyTitle, String message, MessageType type, LocalDateTime createdAt,
-                          Sender sender) {
+public record ChatMessage(Long id, Long partyId, String partyTitle, String message, MessageType type,
+                          LocalDateTime createdAt, Sender sender) {
 
     public static ChatMessage from(Chat chat) {
+        Party party = chat.getParty();
         return ChatMessage.builder()
-                .partyId(chat.getParty().getId())
-                .partyTitle(chat.getParty().getTitle())
+                .id(chat.getId())
+                .partyId(party.getId())
+                .partyTitle(party.getTitle())
                 .message(chat.getMessage())
                 .type(chat.getType())
                 .createdAt(chat.getSentAt())
